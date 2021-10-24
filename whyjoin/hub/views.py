1841714f80fba7a7c1d2dba684bd9be54582d96c
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-24 19:38:10 +0900

    replaced httpresponse with render method

--- whyjoin/hub/views.py
+++ whyjoin/hub/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 
 
 def index(request):
-    return HttpResponse("Index Page")
+    return render(request, "hub/index.html")
 
 
 def about(request):
-    return HttpResponse("About page")
+    return render(request, "hub/about.html")
