8da1c97f06847f5ee9d9cc8d211ebe2ef82e8046
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-24 19:53:04 +0900

    added docstrings

--- whyjoin/hub/views.py
+++ whyjoin/hub/views.py
@@ -1,9 +1,12 @@
+""" This module contains all of the views for the Hub app """
 from django.shortcuts import render
 
 
 def index(request):
+    """ This view serves as the home page for the Why Join App """
     return render(request, "hub/index.html")
 
 
 def about(request):
+    """ This view serves as the about page for the Why Join App """
     return render(request, "hub/about.html")
