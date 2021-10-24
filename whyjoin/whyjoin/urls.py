99068edaab156aa8d2808ff199f251484bc7b3e3
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-24 19:23:54 +0900

    Changed the path for the community app

--- whyjoin/whyjoin/urls.py
+++ whyjoin/whyjoin/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path("", include("community.urls")),
+    path("community/", include("community.urls")),
     path('admin/', admin.site.urls),
 ]
