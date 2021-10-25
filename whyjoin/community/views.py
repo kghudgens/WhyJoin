9ebb8770e6b6ca74d91ed1c242210e83ecd2a2c5
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-25 22:11:19 +0900

    rendered the community template

--- whyjoin/community/views.py
+++ whyjoin/community/views.py
@@ -1,6 +1,6 @@
+""" Views for the community application """
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def community(request):
-    return HttpResponse("Welcome yall")
+    return render(request, "community.html")
