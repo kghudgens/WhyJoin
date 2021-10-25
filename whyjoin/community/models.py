10dc20a7dfc476d128ea97c544366d44e0f5c0f2
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-25 22:05:41 +0900

    created the forum model

--- whyjoin/community/models.py
+++ whyjoin/community/models.py
@@ -1,5 +1,6 @@
 """ The models representing all the data in the community application. """
 from django.db import models
+from django.contrib.auth.models import User
 
 branch_choices = [("USMC", "United States Marine Corps"),
                   ("USA", "United States Army"),
@@ -22,3 +23,18 @@ class CommunityBranch(models.Model):
     purpose = models.TextField()
     # number of users that are interested in hearing about the branch
     member_count = models.IntegerField()
+
+    def __str__(self):
+        return self.name_of_branch
+
+
+class CommunityForums(models.Model):
+    """
+    Model represents discussions created by users. Referencing the branch model
+    to categorize each for its respective branch.
+    """
+    # Reference the built in django user model
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
