5c362707e6490c22ea7b461e67f2dd22842c173b
Kevin Hudgens <hudgens1073@gmail.com>
2021-10-29 21:12:29 +0900

    registered the community models

--- whyjoin/community/admin.py
+++ whyjoin/community/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+""" This module registers selected models in the admin site for model manipulation """
 
+from django.contrib import admin
+from .models import CommunityBranch, CommunityForums
 # Register your models here.
+
+admin.site.register(CommunityForums)
+admin.site.register(CommunityBranch)
