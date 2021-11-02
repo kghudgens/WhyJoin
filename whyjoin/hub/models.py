7b08222765a5bd5fd936123fefef036d161e7062
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-02 20:33:28 +0900

    Created the post model

--- whyjoin/hub/models.py
+++ whyjoin/hub/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+""" This module holds all models for the Hub Application """
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Post(models.Model):
+    """ Model represents the blog posts created by authorized users """
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    # Using the timezone library, can give the users post an automatic time
+    created_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return f'Author : {self.author} Post : {self.title}'
+
+
+class Comments(models.Model):
+    """ Model represents the comments for its respective Post """
