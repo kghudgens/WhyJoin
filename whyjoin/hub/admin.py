9b6c730295994b96b67426a2b11a60ed3425d924
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-02 20:43:56 +0900

    registered the models for the hub app

--- whyjoin/hub/admin.py
+++ whyjoin/hub/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+""" This module registers the created models with the admin site """
 
-# Register your models here.
+from django.contrib import admin
+from .models import Comments, Post
+
+# Registered the models on the admin page
+admin.site.register(Post)
+admin.site.register(Comments)
