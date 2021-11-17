f6fb7d2f222013d1c86cb530b593da2d942e05ce
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-17 23:40:54 +0900

    Created profile for existing profiles

--- whyjoin/user/signals.py
+++ whyjoin/user/signals.py
@@ -5,7 +5,7 @@ of user profiles
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Profile
 
 
@@ -17,4 +17,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except ObjectDoesNotExist:
+        Profile.objects.create(user=instance)
