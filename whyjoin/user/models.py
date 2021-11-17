787a236590dc7c8bd23d4c8b5347ea1cbe69e6a3
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-17 23:28:43 +0900

    Removed the signal logic from the profile model

--- whyjoin/user/models.py
+++ whyjoin/user/models.py
@@ -12,16 +12,5 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
 
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        """ Method creates profile at the same time as new user registers """
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        """ Method saves the profile created in the Profile model """
-        instance.profile.save()
-
     def __str__(self):
         return f'{self.user.username} Profile'
