0eadfbcf42b7691d578c8817db24beb90ee76a12
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-23 22:09:28 +0900

    created path for post view

--- whyjoin/hub/urls.py
+++ whyjoin/hub/urls.py
@@ -1,9 +1,10 @@
 """ The URL configuration for the Hub Application """
 from django.urls import path
 
-from . import views
+from hub.views import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name="about")
+    path("about/", views.about, name="about"),
+    path("blog", views.PostListView.as_view())
 ]
