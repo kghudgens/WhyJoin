b3a5a94eb2824bca380120ff12c0cc69fd1fa881
Kevin Hudgens <hudgens1073@gmail.com>
2021-11-29 23:29:39 +0900

    added url for register view

--- whyjoin/user/urls.py
+++ whyjoin/user/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/<int:user.id>', views.profile, name='profile')
+    # path('profile/<int:user.id>', views.profile, name='profile'),
+    path("user/register", views.register_request, name='register')
 ]
