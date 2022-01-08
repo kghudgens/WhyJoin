aebbb8badc9602be94272c764b3a5aa3236ba548
Kevin Hudgens <hudgens1073@gmail.com>
2022-01-08 17:47:11 +0900

    altered the post model to accept images

--- whyjoin/hub/models.py
+++ whyjoin/hub/models.py
@@ -7,22 +7,24 @@ from django.utils import timezone
 
 class Post(models.Model):
     """ Model represents the blog posts created by authorized users """
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     # Using the timezone library, can give the users post an automatic time
     created_date = models.DateTimeField(default=timezone.now())
+    images = models.ImageField(upload_to="images/")
 
     class Meta:
         ordering = ["-created_date"]
 
     def __str__(self):
-        return f'Author : {self.author} Post : {self.title}'
+        return f"Author : {self.author} Post : {self.title}"
 
 
 class Comments(models.Model):
     """ Model represents the comments for its respective Post """
+
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     name = models.CharField(max_length=60)
     email = models.EmailField()
@@ -30,7 +32,7 @@ class Comments(models.Model):
     created_date = models.DateTimeField(default=timezone.now())
 
     class Meta:
-        ordering = ['created_date']
+        ordering = ["created_date"]
 
     def __str__(self):
         return f"Comment {self.name} on Post {self.post}"
